cca5ca6be3703cd01b916c5ba81973c014e46363
warminski <35105074+warminski@users.noreply.github.com>
2023-03-19 17:32:02 +0100

    convert to object oriented

--- main.py
+++ main.py
@@ -2,52 +2,70 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# scrape past matches and results
 
+class ResultsScraper:
+    def __init__(self):
+        self.url = "https://hltv.org/results"
+        self.results_df = pd.DataFrame(columns=['team_1', 'team_2', 'score_1', 'score_2', 'event', 'match_type'])
 
-
-def scrape_one_page(url):
-    html = requests.get(url)
-    doc = BeautifulSoup(html.content, "lxml")
-    results = doc.find_all("div", {"class": "result-con"})
-    match_data = []
-    df_results = pd.DataFrame(columns=['team1_name', 'team2_name', 'team1_score', 'team2_score', 'event', 'type'])
-    for i in results:
-        tr_tags = i.find_all("tr")
-        for tr in tr_tags:
-            td_tags = tr.find_all("td")
-            for td in td_tags:
-                td_data = (td.get_text().strip())
-                match_data.append(td_data)
-            match_data[1] = match_data[1].split(' - ')
-            match_data.insert(3, match_data[1][0])
-            match_data.insert(4, match_data[1][1])
-            match_data.pop(1)
-            new_df = pd.DataFrame([match_data], columns=df_results.columns)
-            df_results = pd.concat([df_results, new_df], ignore_index=True)
+    def scrape_results_page(self):
+        page = requests.get(self.url)
+        soup = BeautifulSoup(page.content, 'lxml')
+        results_list = soup.find_all('div', {'class': 'result-con'})
+        results = []
+        for result in results_list:
             match_data = []
-    return df_results
+            rows = result.find_all('tr')
+            for row in rows:
+                columns = row.find_all('td')
+                for col in columns:
+                    data = col.get_text().strip()
+                    match_data.append(data)
+                score = match_data[1].split(' - ')
+                match_data.insert(3, score[0])
+                match_data.insert(4, score[1])
+                match_data.pop(1)
+                results.append(match_data)
+                match_data = []
+        self.results_df = pd.DataFrame(results, columns=self.results_df.columns)
+        return self.results_df
+
+    def get_num_pages(self):
+        page = requests.get(self.url)
+        soup = BeautifulSoup(page.content, 'lxml')
+        num_pages = soup.find('span', {'class': 'pagination-data'}).get_text().split()[-1]
+        rounded_num_pages = (int(num_pages) // 100) * 100
+        return rounded_num_pages
+
+    def scrape_all_results_pages(self):
+        first_page_results = self.scrape_results_page()
+        results = pd.concat([self.results_df, first_page_results], ignore_index=True)
+        pages = self.get_num_pages()
+        for i in range(100, pages, 100):
+            self.url = self.url + f'?offset={i}'
+            next_page_results = self.scrape_results_page()
+            results = pd.concat([results, next_page_results], ignore_index=True)
+            print(f"scraped page number {i}")
+        results.to_json('results.json', indent=4)
+        return results
 
 
-# scrape number of pages
-def get_number_of_pages(url):
-    html = requests.get(url)
-    doc = BeautifulSoup(html.content, "lxml")
-    number_of_pages = doc.find("span", {"class": "pagination-data"}).get_text().split()[-1]
-    rounded_number_of_pages = (int(number_of_pages) // 100) * 100
-    return rounded_number_of_pages
+class PlayerScraper:
+    def __init__(self, url):
+        self.url = url
+        self.player_info_df = pd.DataFrame(columns=['nickname', 'name', 'second_name', 'age', 'team'])
+
+    def scrape_player_info(self):
+        page = requests.get(self.url)
+        soup = BeautifulSoup(page.content, 'lxml')
+        # implement player info scraping logic here
+        return soup
+
+    def save_player_info(self):
+        # implement saving player info to file logic here
+        pass
 
 
-pages = get_number_of_pages("https://hltv.org/results")
-
-
-df = pd.DataFrame(columns=['team1_name', 'team2_name', 'team1_score', 'team2_score', 'event', 'type'])
-for i in range(100, pages, 100):
-    url = "https://hltv.org/results"
-    new_url = url + f"?offset={i}"
-    new_df = scrape_one_page(new_url)
-    df = pd.concat([df, new_df], ignore_index=True)
-    print(f"scraped page number {i}")
-
-print(df)
-
+results_scraper = ResultsScraper()
+results_df = results_scraper.scrape_all_results_pages()
+print(results_df)
